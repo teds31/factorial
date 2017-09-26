1c892de081c6e6630d51f8c4db86c8beb57cf8cf
Teddy Stanowski <tstanowski31@gmail.com>
2017-09-26 11:24:00 -0700

    factorial method with benchmark

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,1 +1,17 @@
 # YOUR CODE HERE
+
+require 'benchmark'
+
+def factorial(n)
+  if n <= 1
+    1
+  else
+    n * factorial(n - 1)
+  end
+end
+
+puts Benchmark.measure { "a"*1_000_000 }
+puts factorial(3)
+puts factorial(4)
+puts factorial(5)
+puts factorial(1000)
